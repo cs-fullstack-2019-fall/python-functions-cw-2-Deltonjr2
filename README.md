41481c8c8294353a50daacad993375aae3c75d62
autumn-ragland <raglanda998@gmail.com>
2019-08-26 13:45:28 -0500

    Graded 08-26-2019 13:44:48

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # python-functions-cw_2
 
+You code did not run out of the box. See trying.py for comments. Score : 1
 
 ### Problem 1:
 Create a function that will ask the user for a number. Use the function to get two numbers from the user, then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
--- trying.py
+++ trying.py
@@ -4,12 +4,15 @@
 #
 # Create a function that will ask the user for a number. Use the function to get two numbers from the user, then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
 
+# !! : "Create a function that will ask the user for a number". This is not a function 
 userInput1 = int(input("Enter a number"))
 userInput2 = int(input("Enter a number"))
-operation = (userInput1, userInput2)
+operation = (userInput1, userInput2) # !! : This is not the correct syntax to call a function 
 
 print(userInput1 + userInput2)
 # /Addition
+
+# !! : "Add, subtract, multiple, and divide the numbers." This problem does not call for conditionals 
 
 if operation == '+':
     print ('{10}+ {10} = ' .f(userInput1, userInput2))
