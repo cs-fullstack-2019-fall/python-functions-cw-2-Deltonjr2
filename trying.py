b7b1676345a6f38b9b27dd599b2cc3db5776fdc2
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-26 12:57:00 -0500

    function cw

---
+++ trying.py
@@ -0,0 +1,29 @@
+
+# python-functions-cw_2
+# Problem 1:
+#
+# Create a function that will ask the user for a number. Use the function to get two numbers from the user, then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
+
+userInput1 = int(input("Enter a number"))
+userInput2 = int(input("Enter a number"))
+operation = (userInput1, userInput2)
+
+print(userInput1 + userInput2)
+# /Addition
+
+if operation == '+':
+    print ('{10}+ {10} = ' .f(userInput1, userInput2))
+print(userInput1 + userInput2)
+
+elif operation == '-':
+print ('{10}+ {10} = ' .f(userInput1, userInput2))
+print(userInput1 - userInput2)
+
+elif operation == '*':
+print ('{10}+ {10} = ' .f(userInput1, userInput2))
+print(userInput1 * userInput2)
+
+elif operation == '/':
+print ('{10}/ {10} = ' .f(userInput1, userInput2))
+print(userInput1 / userInput2)
+
